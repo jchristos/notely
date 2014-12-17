61e81a1e318891200fe5c75c346b519eb18a941b
Jennifer Christos <jennifer.christos3@gmail.com>
2014-12-17 16:50:32 -0500

    Notes

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -9,6 +9,28 @@ angular.module('myApp.notes', ['ngRoute'])
   });
 }])
 
-.controller('NotesController', [function() {
+.controller('NotesController', function($scope, $http) {
+  var apiBasePath = 'https://elevennote-nov-2014.herokuapp.com/api/v1/';
+  var postNotePath = apiBasePath + 'notes';
+  var apiKey = '$2a$10$ZDfkfFq7JtH4CK3w3DGkTeTOWK0XmLoGvUSew0DrEcynVPGfv8lei';
+  var notes = [];
 
-}]);
+  $http.get(apiBasePath + 'notes.json?api_key=' + apiKey).success(function(noteData) {
+    $scope.notes = noteData;
+  });
+
+  $scope.commit = function() {
+    $http.post(postNotePath, {
+      api_key: apiKey,
+      note: {
+        title: 'COOL TITLE',
+        body_html: 'LESS COOL BODY'
+      }
+    }).success(function(successData) {
+      console.log(successData);
+    }).error(function(errorData) {
+      console.log(errorData);
+    });
+  };
+
+});
