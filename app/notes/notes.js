0e5e21ad746644e3dc2995b7009508fe068ddf3b
Jennifer Christos <jennifer.christos3@gmail.com>
2014-12-17 18:04:46 -0500

    Adding new notes to the top

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,36 +1,54 @@
 'use strict';
 
-angular.module('myApp.notes', ['ngRoute'])
+var noteApp = angular.module('myApp.notes', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
+noteApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/notes', {
     templateUrl: 'notes/notes.html',
     controller: 'NotesController'
   });
-}])
+}]);
 
-.controller('NotesController', function($scope, $http) {
+noteApp.service('NotesBackend', function($http) {
+
   var apiBasePath = 'https://elevennote-nov-2014.herokuapp.com/api/v1/';
   var postNotePath = apiBasePath + 'notes';
   var apiKey = '$2a$10$ZDfkfFq7JtH4CK3w3DGkTeTOWK0XmLoGvUSew0DrEcynVPGfv8lei';
   var notes = [];
 
-  $http.get(apiBasePath + 'notes.json?api_key=' + apiKey).success(function(noteData) {
-    $scope.notes = noteData;
-  });
+  this.getNotes = function() {
+    return notes;
+  };
 
-  $scope.commit = function() {
+  this.fetchNotes = function() {
+    $http.get(apiBasePath + 'notes.json?api_key=' + apiKey).success(function(noteData) {
+      notes = noteData;
+    });
+  };
+
+  this.postNote = function(note) {
     $http.post(postNotePath, {
       api_key: apiKey,
       note: {
-        title: 'COOL TITLE',
-        body_html: 'LESS COOL BODY'
+        title: note.title,
+        body_html: note.body_html
       }
-    }).success(function(successData) {
-      console.log(successData);
-    }).error(function(errorData) {
-      console.log(errorData);
+    }).success(function(noteData) {
+      notes = [noteData].concat(notes);
     });
+  };
+
+});
+
+noteApp.controller('NotesController', function($scope, $http, NotesBackend) {
+  NotesBackend.fetchNotes();
+
+  $scope.notes = function() {
+    return NotesBackend.getNotes();
+  }
+
+  $scope.commit = function() {
+    NotesBackend.postNote($scope.note);
   };
 
 });
